4a2b35858466121110f038b347bb8dc77f0b34f7
Leo <Aqu4men@gmail.com>
2021-10-06 16:02:46 +0200

    jump done

--- Platinum Project/Assets/Scripts/Controler.cs
+++ Platinum Project/Assets/Scripts/Controler.cs
@@ -4,6 +4,7 @@ using UnityEngine.InputSystem;
 public class Controler : MonoBehaviour
 {
     public float movementSpeed = 6;
+    float distToGround;
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
 
@@ -23,25 +24,30 @@ public class Controler : MonoBehaviour
     public void OnJump(InputAction.CallbackContext context)
     {
         jump = context.action.triggered;
-        Debug.Log("jump");
+        
     }
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
     }
-
+    private void Start()
+    {
+        distToGround = GetComponent<BoxCollider>().bounds.extents.y;
+    }
     void Update()
     {
         // Mouvements du personnage
 
         var movement = MovementInput.x;
         transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * movementSpeed;
-
-        if (jump)
+        Debug.Log(IsGrounded());
+        if (jump && IsGrounded())
         {
-            _rigidbody.AddForce(new Vector3(0, 10,0));
+            Debug.Log("jump");
+            _rigidbody.AddForce(new Vector3(0, 100,0));
         }
+        //Debug.DrawLine(transform.position, /*transform.position + */(-Vector3.up * distToGround),Color.red);
 
         // Rotation du personnage
 
@@ -53,5 +59,15 @@ public class Controler : MonoBehaviour
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
         }
+    }
+
+    bool IsGrounded()
+    {
+        return Physics.Raycast(gameObject.transform.position - Vector3.up * (distToGround + 0.1f), -Vector3.up, 0.1f);
+    }
+   private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + (-Vector3.up * distToGround));
     }
 }
