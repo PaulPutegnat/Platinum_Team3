4b0ac2de50135c1c5ad100b910c2edea4f25dcbd
Leo <Aqu4men@gmail.com>
2021-10-20 10:40:23 +0200

    try fix controler

--- Platinum Project/Assets/Scripts/Controler.cs
+++ Platinum Project/Assets/Scripts/Controler.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -46,7 +47,7 @@ public class Controler : MonoBehaviour
     void Update()
     {
         
-        //Mouvements du personnage
+        /*//Mouvements du personnage
 
           var movement = _movementInput.x;
         if (_movementInput.x < 0.1 && _movementInput.x > -0.1)
@@ -90,6 +91,70 @@ public class Controler : MonoBehaviour
         if (_rigidbody.velocity.y < 0)
         {
             _rigidbody.velocity += new Vector3(0,Physics.gravity.y*GravMultiplier*Time.deltaTime,0);
+        }
+
+
+
+        //Debug.DrawLine(transform.position, /*transform.position + (-Vector3.up * _distToGround),Color.red);
+
+        // Rotation du personnage
+
+        Vector3 direction = new Vector3(movement, 0f, 0f).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+        }*/
+    }
+
+    private void FixedUpdate()
+    {
+        //Mouvements du personnage
+
+        var movement = _movementInput.x;
+        if (_movementInput.x < 0.1 && _movementInput.x > -0.1)
+        {
+            _movementInput.x = 0f;
+            _rigidbody.velocity = new Vector3(_rigidbody.velocity.x / 2, _rigidbody.velocity.y, _rigidbody.velocity.z);
+
+        }
+
+        if (movement != 0)
+        {
+            Acceleration += Accel;
+            Acceleration = Mathf.Clamp(Acceleration, 0, 1f);
+            Debug.Log(Acceleration);
+        }
+        else
+        {
+            Acceleration = 0f;
+        }
+
+        _rigidbody.velocity = new Vector3(movement * ((movementSpeed * Acceleration)), _rigidbody.velocity.y, 0);
+
+
+        //JUMP
+        if (IsGrounded())
+        {
+            CT = Initial_CT;
+        }
+        else
+        {
+            CT -= Time.deltaTime;
+        }
+
+        if (jump && (IsGrounded() || CT > 0))
+        {
+            //Vector2.up * Physics.gravity.y * (fallMultiplierFloat - 1) * Time.deltaTime;
+            _rigidbody.velocity = new Vector3(_rigidbody.velocity.x, jumpForce, 0);
+            CT = 0;
+        }
+
+        if (_rigidbody.velocity.y < 0)
+        {
+            _rigidbody.velocity += new Vector3(0, Physics.gravity.y * GravMultiplier, 0);
         }
 
 
