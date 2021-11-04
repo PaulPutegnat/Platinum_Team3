9bc221d58d606005d5d45bb0368793590e729fca
Leo <Aqu4men@gmail.com>
2021-11-04 16:23:27 +0100

    tobogan Ouiiiiiiiiiiii

---
+++ Platinum Project/Assets/Scripts/Inputs/CollideTobogan.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollideTobogan : MonoBehaviour
+{
+    private Rigidbody rbRigidbody;
+    private bool isGrounded;
+
+    [Range(0.0f, 50f)]
+    public float AccelerationTobogan;
+
+    [Range(0.0f, 50f)]
+    public float MaxSpeedTobogan;
+    private float VelY;
+    private float VelX;
+
+    private void Awake()
+    {
+        rbRigidbody = GetComponent<Rigidbody>();
+    }
+
+
+    private void OnCollisionStay(Collision other)
+    {
+
+        
+        if (other.transform.tag == "tobogan")
+        {
+            Debug.Log("ouiiiiiii");
+            GetComponent<TESTCONTROLER>().enabled = false;
+
+            VelY = rbRigidbody.velocity.y;
+            VelX = rbRigidbody.velocity.x;
+            /*Mathf.Clamp(rbRigidbody.velocity.y, -7, -1);
+            Mathf.Clamp(rbRigidbody.velocity.x, -10, 10);*/
+            if(rbRigidbody.velocity.magnitude < MaxSpeedTobogan)
+                rbRigidbody.velocity = new Vector3(VelX * (1 + AccelerationTobogan / 1000), VelY * (1 + AccelerationTobogan / 1000), 0);
+            
+        }
+
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.transform.CompareTag("tobogan"))
+        {
+            GetComponent<TESTCONTROLER>().enabled = true;
+        }
+    }
+}
