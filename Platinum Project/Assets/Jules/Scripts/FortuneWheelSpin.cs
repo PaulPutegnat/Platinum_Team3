429552e2212d676b106457f36a808f08e68d12f6
JulesFritz <jules.fritz@edu.devinci.fr>
2021-11-10 12:29:16 +0100

    Portal Script

--- Platinum Project/Assets/Jules/Scripts/FortuneWheelSpin.cs
+++ Platinum Project/Assets/Jules/Scripts/FortuneWheelSpin.cs
@@ -86,14 +86,16 @@ public class FortuneWheelSpin : MonoBehaviour
             GameObject spamFameGameObject = Instantiate(SpamQTEGame, Vector3.zero, Quaternion.identity, canvas);
             GameGenerator.gameList.Add(spamFameGameObject);
             spamFameGameObject.transform.localPosition = Vector3.zero;
-            Destroy(this.parent.gameObject);
+            Destroy(this.transform.parent.gameObject);
+            
         }
         else //odd
         {
             GameObject slidGameObject = Instantiate(SlidingBarGame, Vector3.zero, Quaternion.identity, canvas);
             GameGenerator.gameList.Add(slidGameObject);
             slidGameObject.transform.localPosition = Vector3.zero;
-            Destroy(this.parent.gameObject);
+            Destroy(this.transform.parent.gameObject);
+
         }
     }
 
