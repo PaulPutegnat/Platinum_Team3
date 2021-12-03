55b08cde22a1610be6bfb12eaf6597651dc89f9a
Paul Pgt <paul.putegnat@edu.devinci.fr>
2021-12-03 17:32:48 +0100

    Continued broken screen and began cameraspeed

--- Platinum Project/Assets/Scripts/TrapsEffects.cs
+++ Platinum Project/Assets/Scripts/TrapsEffects.cs
@@ -9,8 +9,11 @@ public class TrapsEffects : MonoBehaviour
     public GameObject BrokenScreen;
     public GameObject Rock;
 
-    private float _timer = 0;
-    private float _percent;
+    private TestCam _testcam;
+    private float _duration = 1f;
+    private float _elapsedTime;
+    private float _percentageComplete;
+    private bool _testRock = false;
     private Vector3 _rockStart;
     private Vector3 _rockTarget = new Vector3(0,0,0);
 
@@ -24,28 +27,45 @@ public class TrapsEffects : MonoBehaviour
 
     private void Start()
     {
+        _testcam = FindObjectOfType<Camera>().GetComponent<TestCam>();
         _rockStart = Rock.transform.position;
-        _timer += Time.deltaTime;
-        _percent = _timer / 2f;
     }
 
     void Update()
     {
+        _percentageComplete = _elapsedTime / _duration;
+        Rock.SetActive(true);
+        Rock.transform.position = Vector3.Lerp(_rockStart, _rockTarget, _percentageComplete);
+        
         if (Input.GetKey(KeyCode.A))
         {
-            BrokenScreenEffect();
+            _testRock = true;
+            CameraSpeed();
+        }
+    }
+    
+    public void BrokenScreenEffect()
+    {
+        if (_testRock)
+        {
+            _elapsedTime = Time.deltaTime;
+            _percentageComplete = _elapsedTime / _duration;
+            Rock.SetActive(true);
+            Rock.transform.position = Vector3.Lerp(_rockStart, _rockTarget, _percentageComplete);
+
+            if (_rockStart == _rockTarget)
+            {
+                BrokenScreen.SetActive(true);
+                Debug.Log("Broken!");
+                _testRock = false;
+            }
         }
     }
 
-    public void BrokenScreenEffect()
+    public void CameraSpeed()
     {
-        Rock.SetActive(true);
-        Rock.transform.position = Vector3.Lerp(_rockStart,_rockTarget, _percent);
-
-        if (_rockStart == _rockTarget)
-        {
-            BrokenScreen.SetActive(true);
-            Debug.Log("Broken!");
-        }
+        int currentStep = _testcam.i;
+        float currentStepDuration = _testcam.PointsInfos[currentStep].StepDuration;
+        currentStepDuration = currentStepDuration / 2;
     }
 }
